24be7a3a85439b51a23247afb11ef2a1dc11224a
Robert Martins <robert.rm131@gmail.com>
2021-07-29 11:53:15 -0300

    primeiro exercício resolvido

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/exercicio-primeiros-passos-x1/scripts.js
@@ -0,0 +1,12 @@
+const myName = "Robert";
+const birthCity = "Porto Alegre";
+let brithYear = 1999;
+
+console.log("Nome: ", myName, " Cidade: ", birthCity, " Ano: ", brithYear);
+
+brithYear = 2030;
+
+console.log(brithYear);
+
+birthCity = "Natal";
+//Erro proposital a pedido do exercício. tipo primitivo de constante não pode ser alterada.
