b16af49207574dc73bba6ff183490f69f5d7f77e
Robert Martins <robert.rm131@gmail.com>
2021-08-21 18:28:54 -0300

    adicionar LI gerado em JS, adicionado a ul#days, com classes day + holiday ou friday ou nada.

---
+++ fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-3-eventos/exercicio-eventos-parte2/exercicio-eventos-p2-calendario-trybe/script.js
@@ -0,0 +1,47 @@
+function createDaysOfTheWeek() {
+  const weekDays = ['Domingo', 'Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta', 'Sábado']
+  const weekDaysList = document.querySelector('.week-days');
+
+  for (let index = 0; index < weekDays.length; index += 1) {
+    const days = weekDays[index];
+    const dayListItem = document.createElement('li');
+    dayListItem.innerHTML = days;
+
+    weekDaysList.appendChild(dayListItem);
+  };
+};
+
+function checarFeriado(elemento) {
+  let feriados = ['24', '25', '31'];
+  for (let i = 0; i < feriados.length; i += 1) {
+    if (elemento.innerText === feriados[i]) {
+      return " holiday";
+    }
+  }
+  return '';
+}
+
+function checarSextaFeira(elemento) {
+  let sextas = ['4', '11', '18', '25'];
+  for (let i = 0; i < sextas.length; i += 1) {
+    if (elemento.innerText === sextas[i]) {
+      return " friday";
+    }
+  }
+  return '';
+}
+
+createDaysOfTheWeek();
+  const ul = document.getElementById('days');
+  const dezDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];;
+
+  for (let i = 0; i < dezDaysList.length; i += 1) {
+    let li = document.createElement('li');
+    li.innerText = dezDaysList[i];
+    li.className = "day";
+    li.className += checarFeriado(li);
+    li.className += checarSextaFeira(li);
+    ul.appendChild(li);
+  }
+
+// Escreva seu código abaixo.
