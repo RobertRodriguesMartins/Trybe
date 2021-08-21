5c7a992d8dc00a4bf3251314610b8de57d45ec87
Robert Martins <robert.rm131@gmail.com>
2021-08-21 20:32:13 -0300

    adicionar button#btn-holiday em div.buttons-container

--- fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-3-eventos/exercicio-eventos-parte2/exercicio-eventos-p2-calendario-trybe/script.js
+++ fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-3-eventos/exercicio-eventos-parte2/exercicio-eventos-p2-calendario-trybe/script.js
@@ -1,15 +1,12 @@
-function createDaysOfTheWeek() {
-  const weekDays = ['Domingo', 'Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta', 'Sábado']
-  const weekDaysList = document.querySelector('.week-days');
 
-  for (let index = 0; index < weekDays.length; index += 1) {
-    const days = weekDays[index];
-    const dayListItem = document.createElement('li');
-    dayListItem.innerHTML = days;
+function criarBtnHoliday(frase) {
+  let div = document.getElementsByClassName('buttons-container')[0];
+  let btn = document.createElement('button');
+  btn.id = 'btn-holiday';
+  btn.innerText = frase;
 
-    weekDaysList.appendChild(dayListItem);
-  };
-};
+  div.appendChild(btn);
+}
 
 function checarFeriado(elemento) {
   let feriados = ['24', '25', '31'];
@@ -31,7 +28,7 @@ function checarSextaFeira(elemento) {
   return '';
 }
 
-createDaysOfTheWeek();
+function fazerListaDiasDoMes() {
   const ul = document.getElementById('days');
   const dezDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];;
 
@@ -43,5 +40,27 @@ createDaysOfTheWeek();
     li.className += checarSextaFeira(li);
     ul.appendChild(li);
   }
+}
 
+function createDaysOfTheWeek() {
+  const weekDays = ['Domingo', 'Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta', 'Sábado']
+  const weekDaysList = document.querySelector('.week-days');
+
+  for (let index = 0; index < weekDays.length; index += 1) {
+    const days = weekDays[index];
+    const dayListItem = document.createElement('li');
+    dayListItem.innerHTML = days;
+
+    weekDaysList.appendChild(dayListItem);
+  };
+}; 
+  
 // Escreva seu código abaixo.
+function chamarTodasAsMainFunctions() {
+  createDaysOfTheWeek();
+  fazerListaDiasDoMes();
+  criarBtnHoliday('Feriados');
+}
+
+chamarTodasAsMainFunctions()
+
