e0b81d0ed66c92d77208913892ac8a12031cd99f
Robert Martins <robert.rm131@gmail.com>
2021-08-29 14:29:08 -0300

    adicionar resolução exercício 1

---
+++ fundamentos/bloco-7-introducao-a-javascript-e-testes-unitarios/exercicio-parte1-1/script.js
@@ -0,0 +1,16 @@
+const testingScope = (escopo) => {
+  if (escopo === true) {
+    let ifScope = 'Não devo ser utilizada fora do meu escopo (if)';
+    ifScope = `${ifScope} ótimo, fui utilizada no escopo !`;
+    console.log(ifScope);
+  } else {
+  let elseScope = 'Não devo ser utilizada fora meu escopo (else)';
+  console.log(elseScope);
+  }
+}
+
+testingScope(true);
+
+// Faça as modificações necessárias na função para que o seu comportamento respeite o escopo no qual cada variável foi declarada.   ok!
+// Modifique a estrutura da função para que ela seja uma arrow function .Ok!
+// Modifique as concatenações para template literals .  ok!
