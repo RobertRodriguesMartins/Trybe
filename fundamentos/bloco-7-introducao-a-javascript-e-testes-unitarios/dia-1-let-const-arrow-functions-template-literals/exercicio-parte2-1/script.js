77fd3880efdfbc622fe88fbf9a42a5ae31f84c0e
Robert Martins <robert.rm131@gmail.com>
2021-08-29 21:32:17 -0300

    resolução exercicio 1 parte 2

---
+++ fundamentos/bloco-7-introducao-a-javascript-e-testes-unitarios/dia-1-let-const-arrow-functions-template-literals/exercicio-parte2-1/script.js
@@ -0,0 +1,19 @@
+//retornar fatorial, onde fatorial de N = N*N-1*N-2... N=1
+
+let fatore = 5;
+
+const fatorar = (numero) => {
+  let valor = numero;
+
+  for (let i = 2; i <= numero - 1; i += 1) {
+   valor = valor * i;
+  }
+
+  return valor;
+}
+
+console.log(fatorar(fatore));
+
+const fatorialDeN = n => (n > 1) ? n * fatorialDeN(n - 1): 1;
+
+console.log(fatorialDeN(4));
