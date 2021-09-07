b59a5076311201366ec10007ca0af346020a1910
Robert Martins <robert.rm131@gmail.com>
2021-09-07 09:52:41 -0300

    retirar tempcoderunner.js

--- fundamentos/bloco-7-introducao-a-javascript-e-testes-unitarios/dia-2-javascript-es6-objects/exercicios-objects-parte1/tempCodeRunnerFile.js
+++
@@ -1,6 +0,0 @@
-if (typeof order === 'object') {
-  //   const objeto = Object.assign(order);
-  //   return 'kk'
-  // } else {
-  //   throw new Error('parâmetro recebido por customerInfo não é objeto');
-  // }  
