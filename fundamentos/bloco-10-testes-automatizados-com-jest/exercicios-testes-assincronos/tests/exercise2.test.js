0633272d688f1bcd5172a3d85dd132b83fa84aa6
Robert Martins <robert.rm131@gmail.com>
2021-09-28 16:13:09 -0300

    exercicio 2 resolvido em exercise2

--- fundamentos/bloco-10-testes-automatizados-com-jest/exercicios-testes-assincronos/tests/exercise2.test.js
+++ fundamentos/bloco-10-testes-automatizados-com-jest/exercicios-testes-assincronos/tests/exercise2.test.js
@@ -5,9 +5,11 @@ describe('testing getUserName function', () => {
     getUserName(1)
       .then((resp) => expect(resp).toBe('Mark'))
   ))
-  // it('should return a error when id doesnt exists in database', async () => {
-  //   expect.assertions(1);
+  it('should return a error when id doesnt exists in database', () => {
+    expect.assertions(1)
 
-  // })
+    return getUserName('qualquercoisa')
+      .catch((error) => expect(error.message).toMatch('User with qualquercoisa not found.'))
+  })
 
 });
