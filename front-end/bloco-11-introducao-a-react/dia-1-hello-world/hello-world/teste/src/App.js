22f99155e37738605d2243d1fd1fd9d22d4950ab
Robert Martins <robert.rm131@gmail.com>
2021-10-04 21:33:15 -0300

    exercicio fixar jsx

---
+++ front-end/bloco-11-introducao-a-react/dia-1-hello-world/hello-world/teste/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+const textJSX = 'Hello, JSX';
+
+const elemento = <h1>{textJSX}</h1>
+
+function App() {
+  return (
+    elemento
+  )
+}
+
+export default App;
